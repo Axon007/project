1eefeec0c305603449470f3730746899969f835f
solletivivek <2100031220@kluniversity.in>
2025-05-27 20:59:20 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ const AppDevelopment = lazy(() => import('./pages/AppDevelopment'));
 const ContactUs = lazy(() => import('./pages/ContactUs'));
 const ARVRServices = lazy(() => import('./pages/ar'));
 const Social = lazy(() => import('./pages/Social'));
-const Demo = lazy(() => import('./pages/demo'));
 
 // Loading component
 const LoadingFallback = () => (
@@ -76,7 +75,6 @@ function AppContent() {
             <Route path="/computer-vision" element={<ARVRServices />} />
             <Route path="/social" element={<Social />} />
             <Route path="/contact" element={<ContactUs />} />
-            <Route path="/demo" element={<Demo />} />
           </Routes>
         </Suspense>
       </AnimatePresence>
